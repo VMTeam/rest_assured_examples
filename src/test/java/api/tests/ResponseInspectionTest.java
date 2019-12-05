79c5332c6d9a950336b1fa617ce0d31accf782e1
vmejinskii <vladimir.mejinskii@zensoft.io>
2019-12-05 17:06:13 +0600

    Added class for Response Inspection tests

---
+++ src/test/java/api/tests/ResponseInspectionTest.java
@@ -0,0 +1,34 @@
+package api.tests;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ResponseInspectionTest {
+
+    private RequestSpecification baseSpec() {
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON.withCharset("UTF-8"))
+                .setBaseUri("http://httpbin.org")
+                .setPort(80)
+                .addFilter(new ResponseLoggingFilter())
+                .addFilter(new RequestLoggingFilter())
+                .build();
+    }
+
+    @Test
+    public void test_01_Method_Headers_Success() {
+        given()
+                .spec(baseSpec())
+                .when().get("/cache")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+
+    }
+}
