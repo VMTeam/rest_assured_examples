de741565cc4d7a791d8c0841bb85bb93c8f100d9
vmejinskii <vladimir.mejinskii@zensoft.io>
2019-12-06 20:33:00 +0600

    Added tests for Response Inspection class

--- src/test/java/api/tests/ResponseInspectionTest.java
+++ src/test/java/api/tests/ResponseInspectionTest.java
@@ -1,14 +1,20 @@
 package api.tests;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertTrue;
 
 public class ResponseInspectionTest {
 
@@ -23,12 +29,78 @@ public class ResponseInspectionTest {
     }
 
     @Test
-    public void test_01_Method_Headers_Success() {
+    public void test_01_Methods_Cache() {
         given()
                 .spec(baseSpec())
                 .when().get("/cache")
                 .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("origin", equalTo("158.181.247.178, 158.181.247.178"),
+                        "url", equalTo("https://httpbin.org/cache"));
+        given()
+                .spec(baseSpec())
+                .headers("If-Modified-Since", "1")
+                .when().get("/cache")
+                .then()
+                .statusCode(HttpStatus.SC_NOT_MODIFIED);
+    }
+
+    @Test
+    public void test_02_Methods_Cache_Value() {
+        given()
+                .spec(baseSpec())
+                .when().get("/cache/")
+                .then()
+                .statusCode(HttpStatus.SC_NOT_FOUND);
+        given()
+                .spec(baseSpec())
+                .pathParam("value", "2")
+                .when().get("/cache/{value}")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("origin", equalTo("158.181.247.178, 158.181.247.178"),
+                        "url", equalTo("https://httpbin.org/cache/2"));
+    }
+
+    @Test
+    public void test_03_Methods_Cache_Etag() {
+        given()
+                .spec(baseSpec())
+                .pathParam("If-None-Match", "2")
+                .when().get("/etag/{etag}")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("origin", equalTo("158.181.247.178, 158.181.247.178"),
+                        "url", equalTo("https://httpbin.org/etag/{etag}"));
+        given()
+                .spec(baseSpec())
+                .when().get("/etag/{etag}")
+                .then()
                 .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void test_03_Methods_Response_Headers() {
+        given()
+                .spec(baseSpec())
+                .queryParam("freeform", "ololosha")
+                .when().get("/response-headers")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("Content-Length", equalTo("95"),
+                        "freeform", equalTo("ololosha"));
+    }
+
+    @Test
+    public void test_04_Methods_Response_Headers() {
+        given()
+                .spec(baseSpec())
+                .queryParam("freeform", "ololosha")
+                .when().post("/response-headers")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("Content-Length", equalTo("95"),
+                        "freeform", equalTo("ololosha"));
 
     }
 }
